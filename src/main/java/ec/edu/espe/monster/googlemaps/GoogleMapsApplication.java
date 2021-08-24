12a010468d7430dbb8b24e7b5c58e886fc997430
dlasso <dlasso@bigdataca.com>
2021-08-24 09:05:08 -0500

    initial commit

---
+++ src/main/java/ec/edu/espe/monster/googlemaps/GoogleMapsApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.monster.googlemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/monster/googlemaps/GoogleMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.monster.googlemaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
